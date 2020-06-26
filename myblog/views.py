1d82a8c581016670a211b26e6033ef76d6635358
Miguel Vargas <mfvg1920@gmail.com>
2020-06-26 17:20:03 -0400

    assig2

--- myblog/views.py
+++ myblog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Welcome to MyBlog, Django project!')
+    return HttpResponse('Welcome to MyBlog, Django project!, including Django admin modifications')
